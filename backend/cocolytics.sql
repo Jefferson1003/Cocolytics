f28ecaaaca42563cc34e612e3ac569e931968643
Jefferson <jefferson@example.com>
2026-01-20 22:29:18 +0800

    Add SQL dump for users table with initial data and structure

---
+++ backend/cocolytics.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 20, 2026 at 02:28 PM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cocolytics`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('user','staff','admin') COLLATE utf8mb4_unicode_ci DEFAULT 'user',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'BULILIT', 'jeffbala@gmail.com', '$2a$10$WXI7ZE.Z5Sx9Y3u0HhAWSumpZKcvjt3WxJrlLWSC2vge1kUh8g79a', 'user', '2026-01-20 14:18:26', '2026-01-20 14:18:26'),
+(2, 'Admin User', 'admin@gmail.com', '$2a$10$Nlqid0N48er781AnPaF5w.6RBxezWdS.xRcky.GnL3Py3YiFhSd1K', 'admin', '2026-01-20 14:26:05', '2026-01-20 14:26:05'),
+(3, 'Staff User', 'staff@gmail.com', '$2a$10$Nlqid0N48er781AnPaF5w.piZ3fdQzM3gF1gpwBTikNZWsOwKpH2S', 'staff', '2026-01-20 14:26:05', '2026-01-20 14:26:05'),
+(4, 'Regular User', 'user@gmail.com', '$2a$10$Nlqid0N48er781AnPaF5w.Cu9T12zWFRK9RFoktQzTVEUkSpAGy3.', 'user', '2026-01-20 14:26:05', '2026-01-20 14:26:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
